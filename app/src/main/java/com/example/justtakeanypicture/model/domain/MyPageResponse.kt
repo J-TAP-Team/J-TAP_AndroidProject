fe3eca053e6226ae4771355a5dcdce7e4400e582
kimjaewon <kjw12qwas@daum.net>
2021-06-25 11:50:13 +0900

    ✨ add [model] MyPageResponse.kt

---
+++ app/src/main/java/com/example/justtakeanypicture/model/domain/MyPageResponse.kt
@@ -0,0 +1,8 @@
+package com.example.justtakeanypicture.model.domain
+
+data class MyPageResponse(
+    val email: String,
+    val name: String,
+    val password: String,
+    val public_id: String
+)

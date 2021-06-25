b2d414ff2477f43b5c100e0694d95c382512bccf
kimjaewon <kjw12qwas@daum.net>
2021-06-25 11:52:14 +0900

    ✨ add [model] UploadPictureResponse.kt

---
+++ app/src/main/java/com/example/justtakeanypicture/model/domain/UploadPictureResponse.kt
@@ -0,0 +1,12 @@
+package com.example.justtakeanypicture.model.domain
+
+import java.time.LocalDateTime
+
+data class UploadPictureResponse(
+    val picture_id : Int,
+    val user_id: Int,
+    val image: String,
+    val filename : String,
+    val description : String,
+    val created_at : String
+)

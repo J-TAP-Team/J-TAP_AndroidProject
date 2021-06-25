b968cbc19df7f1433678ed93b32906006b867fbd
kimjaewon <kjw12qwas@daum.net>
2021-06-25 11:39:05 +0900

    ✨ add BaseViewModel

---
+++ app/src/main/java/com/example/justtakeanypicture/base/BaseViewModel.kt
@@ -0,0 +1,20 @@
+package com.example.justtakeanypicture.base
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.disposables.Disposable
+import org.koin.core.KoinComponent
+
+open class BaseViewModel : ViewModel(), KoinComponent {
+
+    private val compositeDisposable = CompositeDisposable()
+
+    fun addDisposable(disposable: Disposable) = compositeDisposable.add(disposable)
+
+    override fun onCleared() {
+        compositeDisposable.clear()
+        super.onCleared()
+    }
+}
